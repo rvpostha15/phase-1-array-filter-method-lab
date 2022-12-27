df3c81cc6b5d1251ee4be38d779511d0b4f9e63e
Ronald <rvpostha15@yahoo.com>
2022-12-26 23:43:22 -0500

    lab solution

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+    //1. define findMatching
+    //2. 2 arguments
+        //drivers (an array)
+        //a string
+    //3. returns matching list of drivers
+    //4. NOT case sensitive
+let findMatching = (drivers, string) => drivers.filter( myMatch => myMatch.toLowerCase() === string.toLowerCase() )
+
+let fuzzyMatch = (drivers, string) => drivers.filter( myMatch => myMatch.toLowerCase().indexOf( string.toLowerCase()) === 0)
+
+let matchName = (drivers, string) => drivers.filter( (myMatch) => myMatch.name === string)
